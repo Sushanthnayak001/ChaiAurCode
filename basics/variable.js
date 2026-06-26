cb7f6a63e52dd400646d9953234cf5e13c88f660
Sushanthnayak001 <sushanthnayak001@gmail.com>
2026-06-26 04:47:38 +0000

    Add initial variable declarations and strict mode in variable.js and variable1.js

---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "bdhavsd@gmail.com"
+var accountPassword = "5678"
+accountCity = "Bengalore"
+let accountstate;
+
+// accountId = 2 //Not allowed
+
+accountEmail = "agdfga@gmail.com"
+accountPassword = 54651651
+accountCity = "pune"
+
+/*
+prefer not to use the var because of the block scop and the functional scop
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ basics/variable1.js
@@ -0,0 +1,21 @@
+"use strict"; //Treat all the JS code as Newer version
+
+//alert() //we cant use this because we are using the nodejs not the browser
+
+let name = "Sushanth"
+let age = 18
+let letLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//String
+// boolean => True/false
+// null => standalone value
+//Undefined => value did not assigened
+// symbol => to find the uniquness
+
+// Objects
+
+console.log(typeof "sushanth") //it is string
+console.log(typeof undefined) //It is undefined
+console.log(typeof null) //It is a object
