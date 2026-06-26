b4a4cd2f337c5f04484c44f5c4d36f9779fc46ec
Sushanthnayak001 <sushanthnayak001@gmail.com>
2026-06-26 07:59:01 +0000

    Add operation and type conversion examples in operation.js and variable1.js

---
+++ basics/operation.js
@@ -0,0 +1,33 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+let str1 = "hellow"
+let str2 = " world"
+let str3 = str1 + str2 //hellow world
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "3") //33
+
+console.log(+true) //1
+//console.log(true+) //error
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// -----------------comparison ------
+
+// console.log(2 > 1); true
+
+// console.log("2" > 1); true
+// console.log("02" > 1); true
+
+// console.log(null > 0); fasle
+// console.log(null == 0); false
+// console.log(null >= 0); false
+
+// === it will compare the datatype also
+ console.log("2" === 2) //false
--- basics/variable1.js
+++ basics/variable1.js
@@ -16,6 +16,33 @@ let letLoggedIn = false
 
 // Objects
 
-console.log(typeof "sushanth") //it is string
-console.log(typeof undefined) //It is undefined
-console.log(typeof null) //It is a object
+// console.log(typeof "sushanth") //it is string
+// console.log(typeof undefined) //It is undefined
+// console.log(typeof null) //It is a object
+
+
+// --------------------Type Conversion--------------------
+
+let score = "33abc"
+//const {score} = req.body //to recive the requst from the frontend
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber); //It is NaN (it is not a number)
+// console.log(typeof Nan) //type is undefined
+
+//"33" => 33
+// "33abc" => NaN
+//true=>1,fasle=>0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//"" => false
+//"anyname or value" => true
+
+let Number = 33
+let StringNumber = String(Number) //type is String
