3295220d25e88691f51e66aafde6177f9a5086e4
Sushanthnayak001 <sushanthnayak001@gmail.com>
2026-06-28 05:06:11 +0000

    Add datatype summary and examples in datatypeSummary.js

---
+++ basics/datatypeSummary.js
@@ -0,0 +1,33 @@
+ // primitive data type
+
+// 7types
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+// Non primitive(refernce type)
+// Arrays, Objects, Functions
+
+// const score = 100
+
+// const id = Symbol('123');
+// const anotherid = Symbol('123')
+// console.log(id === anotherid)
+
+const heros = ["Shakthiman", "Naagraj" , "Doga"];
+
+let myobj = {
+    name: "Sushanth Nayak",
+    age : 22,
+}
+
+const myfunction = function(){
+    console.log("Hellow world"); 
+}
+
+console.log(typeof myfunction)
